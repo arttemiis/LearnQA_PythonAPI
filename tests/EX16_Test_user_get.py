c7ccd1e05342321f6b2a937f5ee059ef7edd80cb
arttemiis <a.zvezdin@itexpert.ru>
2022-03-22 20:48:46 +0300

    "Ex16 Test user get"

--- tests/EX16_Test_user_get.py
+++ tests/EX16_Test_user_get.py
@@ -14,8 +14,6 @@ class TestUserGet(BaseCase):
         response1 = MyRequests.post("user/login", data=first_data)
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
-        user_id_first = self.get_json_value(response1, "user_id")
-
 
         register_data = self.prepare_registration_data()
         response2 = MyRequests.post("user", data=register_data)
@@ -24,7 +22,6 @@ class TestUserGet(BaseCase):
         user_id_second = self.get_json_value(response2, "id")
 
         response3 = MyRequests.get(f"user/{user_id_second}", headers={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
-
         Assertion.assert_json_has_key(response3, "username")
         Assertion.assert_json_has_not_key(response3, "email")
         Assertion.assert_json_has_not_key(response3, "firstName")
